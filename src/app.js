05d5e92669797c31b309eb58af9aa940683afb6d
Julio-Martel <julioramdom123@gmail.com>
2026-04-11 18:40:48 -0300

    refactor: comenta codigo

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const app = express();
 app.use(express.json());
 
 const adminRoutes = require('../src/routes/admin.routes');
-const medicosRoutes = require('../src/routes/medicos.routes');
-const pacientesRoutes = require('../src/routes/pacientes.routes');
+//const medicosRoutes = require('../src/routes/medicos.routes');
+//const pacientesRoutes = require('../src/routes/pacientes.routes');
 
-app.use('/admin', adminRoutes);
-app.use('/medicos', medicosRoutes);
-//app.use('/pacientes', pacientesRoutes);
+app.use('api/admin', adminRoutes);
+//app.use('api/medicos', medicosRoutes);
+//app.use('api/pacientes', pacientesRoutes);
 
 module.exports = app;
