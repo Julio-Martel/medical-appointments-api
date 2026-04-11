f39a2e04bdcb8697f80be1fd8ac5bb53f136cba2
Julio-Martel <julioramdom123@gmail.com>
2026-04-11 16:48:37 -0300

    feat: arregla

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const app = express();
 
 app.use(express.json());
 
+const adminRoutes = require('../src/routes/admin.routes');
 const medicosRoutes = require('../src/routes/medicos.routes');
 const pacientesRoutes = require('../src/routes/pacientes.routes');
 
+app.use('/admin', adminRoutes);
 app.use('/medicos', medicosRoutes);
-app.use('/pacientes', pacientesRoutes);
+//app.use('/pacientes', pacientesRoutes);
 
 module.exports = app;
