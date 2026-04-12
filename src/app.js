18dc2a0417db3ed0abbb288364e6082da27c6baa
Julio-Martel <julioramdom123@gmail.com>
2026-04-12 18:03:10 -0300

    feat: variables

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const adminRoutes = require('../src/routes/admin.routes');
 //const medicosRoutes = require('../src/routes/medicos.routes');
 //const pacientesRoutes = require('../src/routes/pacientes.routes');
 
-app.use('api/admin', adminRoutes);
+app.use('/api/admin', adminRoutes);
 //app.use('api/medicos', medicosRoutes);
 //app.use('api/pacientes', pacientesRoutes);
 
