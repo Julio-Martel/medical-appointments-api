1cf28c23568d69dd6341d6e6eeff3630549050fb
Julio-Martel <julioramdom123@gmail.com>
2026-04-12 19:13:31 -0300

    feat: compara hash

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,29 @@
+const db = require('../config/db');
+
+const getUsuarios = async(req,res) => {
+    const headerToken = req.headers.authorization;
+
+    if(!headerToken){
+        return res.status(404).json({
+            mensaje: 'Debe iniciar sesion como admistrador para ver todos los usuarios'
+        })
+    }
+
+    try {
+
+        const [rows] = await db.query(`SELECT * FROM Usuarios`);
+
+        res.status(200).json(
+            {
+                mensaje: 'Listado de todos los usuarios',
+                rows: rows
+            })
+
+    } catch(error){
+        res.status(500).json({mensaje:'Error interno del servidor'})
+    }
+}
+
+module.exports = {
+    getUsuarios
+}
