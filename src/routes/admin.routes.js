e2051c889f9504fc3c3771d3bc1f6dcc8bb09ea8
Julio-Martel <julioramdom123@gmail.com>
2026-04-12 20:43:25 -0300

    cambios

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router =  express.Router();
 
 const authController = require('../middlewares/auth.controller');
+const adminController = require('../controllers/admin.controller');
 
+//LOGEO Y REGISTRO DEL ADMINISTRADOR
 router.post('/login',authController.login);
 router.post('/register', authController.register);
+
+router.get('/', adminController.getUsuarios);
+
 
 module.exports = router;
 
