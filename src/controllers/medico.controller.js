193d1b371816da48514a0764e7568be72caf7dd2
Julio-Martel <julioramdom123@gmail.com>
2026-04-15 19:33:12 -0300

    fix: validacion de datos

---
+++ src/controllers/medico.controller.js
@@ -0,0 +1,25 @@
+const db = require('../config/db');
+
+const getTurnos = async(req,res) => {
+    
+    const id = req.usuario.id;
+
+    const [turnosFiltrados] = await db.query(`SELECT * FROM Turnos WHERE medico_id = ?`, [id]);
+
+    if(turnosFiltrados.length === 0){
+        return res.status(404).json({
+            mensaje: 'No tiene turnos asignados'
+        })
+    }
+
+    res.status(200).json({
+        mensaje: 'Todos los turnos que se te asignaron',
+        turnos: turnosFiltrados
+    })
+
+}
+
+
+module.exports = {
+    getTurnos
+}
