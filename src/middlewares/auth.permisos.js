cf5fa9e556efa00470fee06ac4739965bd0b15d9
Julio-Martel <julioramdom123@gmail.com>
2026-04-17 18:37:27 -0300

    refactor: mensaje

--- src/middlewares/auth.permisos.js
+++ src/middlewares/auth.permisos.js
@@ -1,7 +1,7 @@
 const verificarRolPaciente = (req,res,next) => {
 
     if (req.usuario.rol !== 'paciente') {
-        return res.status(403).json({ mensaje: 'Debe ser paciente para realizar ver sus turnos.' });
+        return res.status(403).json({ mensaje: 'Debe ser paciente para realizar esta accion.' });
     }
 
     next();
