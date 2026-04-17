548590f6f57574f0e817031f1918d6809398c983
Julio-Martel <julioramdom123@gmail.com>
2026-04-17 20:36:16 -0300

    fix: ruta

--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../middlewares/auth.controller');
-const authMiddleware = require('../middlewares/authMiddlewares');
+const controlarToken = require('../middlewares/authMiddlewares');
 const permisoPaciente = require('../middlewares/auth.permisos');
 const pacienteController = require('../controllers/paciente.controllers');
 
 //LOGEARSE
 router.post('/login', authController.login);
 
-router.post('/turnos', authMiddleware.authMiddleware, permisoPaciente.verificarRolPaciente,  pacienteController.creatTurno);
-router.get('/turnos', authMiddleware.authMiddleware, permisoPaciente.verificarRolPaciente, pacienteController.getTurnos);
+router.post('/turnos', controlarToken.authMiddleware, permisoPaciente.verificarRolPaciente,  pacienteController.creatTurno);
+router.get('/turnos', controlarToken.authMiddleware, permisoPaciente.verificarRolPaciente, pacienteController.getTurnos);
+router.post('/turnos/:id', controlarToken.authMiddleware, permisoPaciente.verificarRolPaciente, pacienteController.cancelarTurno);
+
 
 
 module.exports = router;
