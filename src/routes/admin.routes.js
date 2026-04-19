47a7dc66d22d5c5eac448f6f6678854276eff90b
Julio-Martel <julioramdom123@gmail.com>
2026-04-19 19:12:49 -0300

    feat: permisos

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,17 +4,18 @@ const router =  express.Router();
 const authController = require('../middlewares/auth.controller');
 const adminController = require('../controllers/admin.controller');
 const authMiddleware = require('../middlewares/authMiddlewares');
+const permisos = require('../middlewares/auth.permisos')
 
 //LOGEO Y REGISTRO DEL ADMINISTRADOR
 router.post('/login',authController.login);
 router.post('/register', authController.register);
 
 //OBTENER TODOS LOS USUARIOS
-router.get('/', authMiddleware.authMiddleware, adminController.getUsuarios);
+router.get('/', authMiddleware.authMiddleware, permisos.verificarRolMedico,  adminController.getUsuarios);
 //ELIMINAR USUARIO
-router.delete('/:id', authMiddleware.authMiddleware, adminController.deleteUsuarios);
+router.delete('/:id', authMiddleware.authMiddleware, permisos.verificarRolMedico, adminController.deleteUsuarios);
 //VER TODOS LOS TURNOS
-router.get('/turnos', authMiddleware.authMiddleware, adminController.getTurnos);
+router.get('/turnos', authMiddleware.authMiddleware, permisos.verificarRolMedico, adminController.getTurnos);
 
 module.exports = router;
 
