545ac1dfe0eccd70bfae84e53533f52c2d5706fa
Julio-Martel <julioramdom123@gmail.com>
2026-04-19 20:21:17 -0300

    fix: next()

--- src/middlewares/auth.permisos.js
+++ src/middlewares/auth.permisos.js
@@ -1,15 +1,32 @@
 const verificarRolPaciente = (req,res,next) => {
 
     if (req.usuario.rol !== 'paciente') {
-        console.log(req.usuario.rol)
         return res.status(403).json({ mensaje: 'Debe ser paciente para realizar esta accion.' });
     }
 
     next();
-
 }
 
+const verificarRolMedico = (req,res,next) => {
+    if(req.usuario.rol !== 'medico'){
+        return res.status(403).json({ mensaje: 'Debe ser medico para realizar esta accion.' });
+    } 
+
+    next();
+}
+
+const verificarRolAdmin = (req,res,next) => {
+     if(req.usuario.rol !== 'admin'){
+        return res.status(403).json({ mensaje: 'Debe ser administrador para realizar esta accion.' });
+    } 
+    
+    next();
+}
+
+
 module.exports = {
-    verificarRolPaciente
+    verificarRolPaciente,
+    verificarRolMedico,
+    verificarRolAdmin
 }
 
