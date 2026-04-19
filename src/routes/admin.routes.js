65e32d8f6f82a29e9ba47a40956057f36d86f7cc
Julio-Martel <julioramdom123@gmail.com>
2026-04-19 20:40:36 -0300

    refactor: comentarios

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -6,15 +6,11 @@ const adminController = require('../controllers/admin.controller');
 const authMiddleware = require('../middlewares/authMiddlewares');
 const permisos = require('../middlewares/auth.permisos')
 
-//LOGEO Y REGISTRO DEL ADMINISTRADOR
 router.post('/login',authController.login);
 router.post('/register', authController.register);
 
-//OBTENER TODOS LOS USUARIOS
 router.get('/', authMiddleware.authMiddleware, permisos.verificarRolAdmin,  adminController.getUsuarios);
-//ELIMINAR USUARIO
 router.delete('/:id', authMiddleware.authMiddleware, permisos.verificarRolAdmin, adminController.deleteUsuarios);
-//VER TODOS LOS TURNOS
 router.get('/turnos', authMiddleware.authMiddleware, permisos.verificarRolAdmin, adminController.getTurnos);
 
 module.exports = router;
